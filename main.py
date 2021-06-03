ce6ab4f99b45f62a79b4c0b483819d8770b7e05e
chosokabe1 <tktkshi101110@gmail.com>
2021-06-04 01:27:22 +0900

    a

---
+++ main.py
@@ -0,0 +1,11 @@
+# 変数
+msg = "Hello World"
+print(msg)
+msg  = "Hello Again"
+print(msg)
+
+# 定数
+ADMIN_EMAIL = "tktkshi101110@gmail.com"
+
+#文字列
+s = "he"
---
+++ myapp.py
@@ -0,0 +1,5 @@
+'''
+comment
+'''
+#comment
+print("Hello World")
